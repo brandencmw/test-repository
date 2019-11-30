b69a58232fd10727a50dc1719cf12a7d227d7612
brandencmw <58235015+brandencmw@users.noreply.github.com>
2019-11-30 16:20:53 -0500

    bubblesort

--- bubblesort.py
+++ bubblesort.py
@@ -10,7 +10,7 @@ def bubble_sort(list):
 
 	return list
 
-list = input("Enter the set of numbers to be sorted separated by spaces.").split()
+list = input("Enter the set of numbers to be sorted separated by spaces: ").split()
 
 list = bubble_sort(list)
 print(list)
