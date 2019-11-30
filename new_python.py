0545c53e7165f3deed4d65ac1556368b58b1f98d
brandencmw <58235015+brandencmw@users.noreply.github.com>
2019-11-30 14:52:06 -0500

    added new_python.py

---
+++ new_python.py
@@ -0,0 +1,15 @@
+def sum_nums(num1, num2):
+	
+	sum = num1 + num2
+
+	return sum
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+sum = sum_nums(num1, num2)
+
+print("The sum is {}".format(sum))
+
+while True:
+	print()
